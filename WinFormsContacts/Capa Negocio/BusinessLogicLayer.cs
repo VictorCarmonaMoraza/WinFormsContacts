d51da3e5b1e7237e865d156b18f308bdc11d09ee
Victor Manuel Carmona Moraza <VictorCarmona9035@hotmail.com>
2022-03-13 21:19:38 +0100

    5 -  Capa Negocio (BusinessLogicLayer)

--- WinFormsContacts/Capa Negocio/BusinessLogicLayer.cs
+++ WinFormsContacts/Capa Negocio/BusinessLogicLayer.cs
@@ -3,10 +3,37 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using WinFormsContacts.Capa_Acceso_Datos;
+using WinFormsContacts.Model;
 
 namespace WinFormsContacts.Capa_Negocio
 {
     public class BusinessLogicLayer
     {
+        private DataAccessLayer _dataAccessLayer;
+
+        public BusinessLogicLayer()
+        {
+            _dataAccessLayer = new DataAccessLayer();
+        }
+
+
+
+        //Metodo para acceder a la capa de acceso a datos
+        public Contact SaveContact(Contact contact)
+        {
+            //El contacto es nuevo
+            if(contact.Id == 0)
+            {
+                //Crearemos un metodo para insertar yq eu llamaremos
+            }
+            //Es un Update
+            else
+            {
+
+            }
+            //para que no de error de momento
+            return contact;
+        }
     }
 }
